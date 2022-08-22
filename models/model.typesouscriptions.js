ba52af4bd708c3214b0d1490027ae583b9e0411a
David Maene <davidmened@gmail.com>
2022-08-22 16:39:36 +0200

    up me line

---
+++ models/model.typesouscriptions.js
@@ -0,0 +1,29 @@
+const Sequelize = require('sequelize');
+const { generateIdentifier } = require('../helpers/helper.random.js');
+const { Configs } = require('../configs/configs.js');
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const Typesouscription = Configs.define('__tbl_type_souscriptions', { 
+    souscription: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    montant: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    currency: {
+        type: Sequelize.STRING,
+        allowNull: true,
+        defaultValue: "USD"
+    },
+    status: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        defaultValue: 1
+    }
+});
+
+module.exports = { Typesouscription };
