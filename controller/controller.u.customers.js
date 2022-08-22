b6ea9ee165a499faf1756ee6d8f0cc42a1b38ee2
David Maene <davidmened@gmail.com>
2022-08-22 17:59:34 +0200

    up me line => DONE

--- controller/controller.u.customers.js
+++ controller/controller.u.customers.js
@@ -2,7 +2,8 @@ const { Response } = require("../helpers/helper.message.server.js")
 
 const ControllerUCustomrs = {
     register: async (req, res, next) => {
-
+        const { fsname, lsname, nickname, email, password, phone_1, phone_2, genre, avatar, nationality, etatcivil, idsouscription } = req.body;
+        if(!fsname || !lsname || !email || !password || !phone_1 || !nationality ) return Response(res, 401, "this request must have at leats !fsname || !lsname || !email || !password || !phone_1 || !nationality ")
     },
     signin: async (req, res, next) => {
 
